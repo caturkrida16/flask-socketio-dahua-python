b6c389f7280c9c51a2e097acc5eccd20794e7283
Catur Krida Cahya Laksana <caturkrida16@gmail.com>
2023-07-05 14:55:06 +0800

    Update main.py
    [Add]
    1. Event "ptz_control" SocketIO to control the PTZ Camera
    
    [Change]
    1. PTZ Control Dahua API to ONVIF

--- main.py
+++ main.py
@@ -15,31 +15,30 @@ import requests
 
 ## PIP
 from flask import Flask, jsonify, request, render_template, Response
-import flask_httpauth
-from werkzeug.security import generate_password_hash, check_password_hash
 import cv2
+from flask_socketio import SocketIO
+from sensecam_control import onvif_control
 
 
 
 ### >>><<< ###
-# Intialization
-## System
-app = Flask(__name__)
-auth = flask_httpauth.HTTPBasicAuth()
+# Initialization
+## Environment Variable
 host = os.getenv("HOST")
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
 
+## Core
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app, async_mode="eventlet", cors_allowed_origins='*')
+cam = onvif_control.CameraControl(host, username, password) # Initialize the ONVIF
+cam.camera_start()
+
 ## Dahua HTTP API (Specify)
 http_api = "http://" + host + "/cgi-bin"
-rtsp_api = "rtsp://" + host + ":554/cam/realmonitor?channel=1&subtype=1"
 reboot = "/magicBox.cgi?action=reboot"
 get_system_info = "/magicBox.cgi?action=getSystemInfo"
-
-# Users
-users = {
-    "admin": generate_password_hash("admin")
-}
 
 
 
@@ -55,29 +54,17 @@ def index():
 @app.route("/stream", methods=["GET"])
 def stream():
     # Connect to RTSP Stream
-    rtsp_stream = cv2.VideoCapture(rtsp_api)
+    rtsp_stream = cv2.VideoCapture("rtsp://" + username + ":" + password + "@" + host + ":554/cam/realmonitor?channel=1&subtype=1")
     return Response(cam_frames(rtsp_stream), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-### Move the Camera View 
-@app.route("/ptz_move", methods=["POST"])
-@auth.login_required
-def ptz_move():
-    data = request.json
-    try:
-    if (data["action"] == "start" or data["action"] == "stop"):
-        if (data["direction"] == "Up" or data["direction"] == "Down" or data["direction"] == "Left" or data["direction"] == "Right" or data["direction"] == "LeftUp" or data["direction"] == "RightUp" or data["direction"] == "LeftDown" or data["direction"] == "RightDown"):
-            if (data["speed"] >= 1 and data["speed"] <= 8):
-                response = api_auth(http_api + "/ptz.cgi?action=" + data["action"] + "&channel=0&code=" + data["direction"] + "&arg1=" + str(data["speed"]) + "&arg2=" + str(data["speed"]) + "arg3=0")
-                return response, 200
-            else:
-                return "Invalid PTZ Speed", 400
-        else:
-            return "Invalid PTZ Direction", 400
-    else:
-        return "Invalid PTZ Action", 400
-    except:
-        return "Your JSON is error", 400
     
+## SocketIO
+### PTZ Control from ONVIF
+@socketio.on("ptz_control")
+def ptz_control(json):
+    print(json)
+    ptz_cam(json["direction"]) # Move Camera
+    cam.stop_move() # And stop it
+
 ## Function
 ### Generate camera frames
 def cam_frames(rtsp):
@@ -85,6 +72,7 @@ def cam_frames(rtsp):
         success, frame = rtsp.read()
         if not success:
             break
+            
         else:
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
@@ -106,16 +94,59 @@ def api_auth(url):
     else:
         print("Unknown authentication type!")
         
-### Auth Verify Password
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
-        return username
+### ONVIF PTZ Control
+def ptz_cam(direction):
+    # Up
+    if (direction == "Up"):
+        cam.continuous_move(0, 1, 0)
 
+    # Left
+    elif (direction == "Left"):
+        cam.continuous_move(-1, 0, 0)
 
+    # Down
+    elif (direction == "Down"):
+        cam.continuous_move(0, -1, 0)
+
+    # Right
+    elif (direction == "Right"):
+        cam.continuous_move(1, 0, 0)
+
+    # Zoom In
+    elif (direction == "ZoomIn"):
+        cam.continuous_move(0, 0, 1)
+
+    # Zoom Out
+    elif (direction == "ZoomOut"):
+        cam.continuous_move(0, 0, -1)
 
 ### >>><<< ###
 # APP Run
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
+
+    
+
+### >>><<< ###
+# Archive
+"""
+### Move the Camera View 
+@app.route("/ptz_move", methods=["POST"])
+@auth.login_required
+def ptz_move():
+    data = request.json
+    try:
+        if (data["action"] == "start" or data["action"] == "stop"):
+            if (data["direction"] == "Up" or data["direction"] == "Down" or data["direction"] == "Left" or data["direction"] == "Right" or data["direction"] == "LeftUp" or data["direction"] == "RightUp" or data["direction"] == "LeftDown" or data["direction"] == "RightDown"):
+                if (data["speed"] >= 1 and data["speed"] <= 8):
+                    response = api_auth(http_api + "/ptz.cgi?action=" + data["action"] + "&channel=1&code=" + data["direction"] + "&arg1=0&arg2=" + str(data["speed"]) + "&arg3=0")
+                    return response, 200
+                else:
+                    return "Invalid PTZ Speed", 400
+            else:
+                return "Invalid PTZ Direction", 400
+        else:
+            return "Invalid PTZ Action", 400
+    except:
+        return "Your JSON is error", 400
+        """
